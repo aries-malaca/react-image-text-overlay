5dfc49b05a3d42914c0a0920f98adee0b2ce67fb
Michael Aries Malaca <malaca.aries29@gmail.com>
2021-11-21 17:18:52 +0800

    remove dependency styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Container } from './styles';
 
 // Delete me
 export const ReactImageText: React.FC<Props> = ({ backgroundImage, textSet }) => {
   const fillText = textSet.concat(" ").repeat(5000);
 
+  const componentStyle = {
+    backgroundImage: `url(${ backgroundImage })`,
+    height: '100vh',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    overflow: 'hidden',
+    fontWeight: 600,
+    backgroundColor: 'black',
+    fontFamily: 'arial'
+  }
+
   return (
-    <Container backgroundImage={ backgroundImage }>
-      <div>{ fillText }</div>
-    </Container>
+    <section style={ componentStyle }>
+      <div style={{ mixBlendMode: 'overlay', color: '#ffffff8c' }}>{ fillText }</div>
+    </section>
   );
 };
+
 
 interface Props {
   backgroundImage?: any;
